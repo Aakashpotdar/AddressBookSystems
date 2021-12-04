eac4af043e0ec4a696aa5bbd57b5719994bbc8b3
aakashpotdar <potdaraakash@gmail.com>
2021-12-04 13:10:46 +0530

    Method to search name by city name is added

--- AddressBook/AddressBookOprations.cs
+++ AddressBook/AddressBookOprations.cs
@@ -63,7 +63,14 @@ namespace AddressBook
             {
                 Console.WriteLine("the City is not present");
             }
-
+            
+        }
+        public void SearchNameByCityName(string CityName)
+        {
+            foreach (Address per in addresses.FindAll(e => (e.city == CityName)).ToList())
+            {
+                Console.WriteLine("Name: " + per.name + " City: " + per.city);
+            }
         }
     }
 }
